6a8dfa49307d9614b3ab5259433145d40ee1353a
Michael Flasco <mcflasco@gmail.com>
2020-11-07 21:54:14 -0500

    added game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,15 +4,21 @@ console.log("Words:", words);
 
 // Variable Declaration
 let displayWord = document.getElementById('blanks');
-let incorrect = document.getElementById('incorrect');
+let displayIncorrect = document.getElementById('incorrect');
+let displayWins = document.getElementById('wins');
+let displayLosses = document.getElementById('losses');
+let outcome = document.getElementById('outcome');
 let incorrectLetters = [];
 let correctLetters = [];
 let guessedLetters = [];
 let selectWord;
-let wins;
-let losses;
+let wins = 0;
+let losses = 0;
+let guesses = 3;
+let correct = 0;
 let games;	
 let letter;
+
 
 // Function to select word from array
 const gameSetup = () =>{
@@ -20,110 +26,82 @@ const gameSetup = () =>{
 	selectWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
 	console.log("Selected Word:",  selectWord);
 	displayBlankWord(selectWord);
-	letterCheck()
-	
+	letterCheck()	
 }
 
 
 // Function that takes the value of the key entered and compares it to the letters of the selected word.
 const letterCheck = () => {
+
 	document.onkeypress = function(evt){
 		letter = evt.key.toUpperCase();
 		guessedLetters.push(letter);
-		// console.log('Guessed letters:', guessedLetters)
+		
 		correctGuess(letter);
 	}
 	return letter;
 }
+
 
 const displayBlankWord = (word) =>{
 
 	for(let i = 0; i < word.length; i++){
 		correctLetters.push("_")
 	}
-
 	displayWord.innerHTML = correctLetters.join(" ");
 	console.log("Correct Letters:", correctLetters)
-	return correctLetters
+
 }
+
 
 const correctGuess = (letter) => {
 
 	if(selectWord.indexOf(letter) < 0){
+
 		incorrectLetters.push(letter)
+		guesses = guesses - 1;
 		incorrect.innerHTML = incorrectLetters.join(" ")
-		console.log("Incorrect letter added:", incorrectLetters)	
+		console.log("Incorrect letter added:", incorrectLetters)		
+
 	} else{
 
 	for(let i = 0; i < selectWord.length; i++){
 		if(selectWord[i] == letter){
 			correctLetters[i] = letter
+			correct = correct + 1;
+			console.log(correct)	
 		}
 	}
 }
+	
 	console.log("Correct letter added:", correctLetters)
 	displayWord.innerHTML = correctLetters.join(" ")
+	gameOutcome();
 }
 
 
+const gameOutcome = () => {
+	if(guesses == 0){
+			losses = losses + 1;
+			displayLosses.innerHTML = losses;
+			outcome.innerHTML = "You lose!";
+			console.log("you lose");
 
+	}
+	else if(correct === correctLetters.length){
+		wins = wins + 1;
+		displayWins.innerHTML = wins;
+		console.log('Mikasa!');
+		outcome.innerHTML = "you won!";
+		correctLetters = []
+		gameSetup();
+		
+	}
 
+}
 
 gameSetup();
 
-
-
-
-
-// Function for inital game setup.
-// const gameSetup = () => {
-
-// 	// Select a random word from array, display selected in word in console.
-	
-// 	console.log('Selected Word:', selectedWord);	
-
-// 	// Loop through the selected word and change HTML to '_' based on how many letters are in the selected word. 
-// 	for(let i = 0; i < selectedWord.length; i++){
-
-	// 		// Loop through correct word and replace letter with _
-	// 		correctLetters.push("_")
-
-// 		// Display blank spaces from array in HTML.
-// 		word.innerHTML = correctLetters.join(" ");
-		
-// 	}
-// 	console.log("Correct Letters:", correctLetters)
-
-
-
-
-
-
-// gameSetup();
-
-
-
-// If word is fully spellled out stop game
-// Add continue button when game ends
-// If gamer wins increment "win count"
-// If game loses decrement "loss count"
-// Add Giphy API for win/lose
-
-
-
-// for(let i = 0; i < selectedWord.length; i++){
-// 		     	if(selectedWord[i] === letter){
-//  		     		correctLetters[i] = letter
-
-// 		     	} 
-// 		     }
-// 		     word.innerHTML = correctLetters.join(" ")		
-
-// 			// Get onkeyup value after button pressed.
-// 			// letter = event.key.toUpperCase();			
-// 			// Push guessed letters to array.
-// 			guessedLetters.push(letter)	
-// 			console.log("Guessed Letters:", guessedLetters)
 
 
 
